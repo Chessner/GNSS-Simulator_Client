edc60ab2b1165474d1aed90e06c3b0f2505e2d9a
Chessner <s2010237006@students.fh-hagenberg.at>
2022-05-30 17:53:53 +0200

    Beidou satellites now with distinct look. Changed some occurrences of "GAGSV" in NMEA-data-3--Materl-Position-Statisch.nmea to "BDGSV" to test Beidou satellites.

--- src/at/fh/hgb/mc/BeidouSat.java
+++ src/at/fh/hgb/mc/BeidouSat.java
@@ -2,7 +2,7 @@ package at.fh.hgb.mc;
 
 import javafx.geometry.VPos;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 
 import java.awt.geom.Point2D;
@@ -11,27 +11,33 @@ public class BeidouSat extends SatelliteInfo{
     @Override
     public void createShape(Pane _parent, double _referenceX, double _referenceY, double _radius) {
         Point2D.Double point = getCoordinates(_radius);
-        Circle circle = new Circle();
-        circle.setId("Satellite");
-        StringBuilder circleStyle = new StringBuilder();
+        Polygon triangle = new Polygon();
+        triangle.setId("Satellite");
+        StringBuilder triangleStyle = new StringBuilder();
 
         if (mSNRdB == Integer.MIN_VALUE) {
             //no snr value exists (marked with Integer.MIN_VALUE)
-            circleStyle.append("-fx-fill: red; ");
+            triangleStyle.append("-fx-fill: red; ");
         } else if (mParentNMEAInfo.mIDsSatellitesUsed.contains(mID)) {
             //satellite was used
-            circleStyle.append("-fx-fill: green; ");
+            triangleStyle.append("-fx-fill: green; ");
         } else {
             //satellite was not used
-            circleStyle.append("-fx-fill: #0000");
-            circleStyle.append(Integer.toHexString((int) Math.max(255, Math.min(0, mSNRdB))));
-            circleStyle.append("ff; ");
+            triangleStyle.append("-fx-fill: #0000");
+            triangleStyle.append(Integer.toHexString((int) Math.max(255, Math.min(0, mSNRdB))));
+            triangleStyle.append("ff; ");
         }
-        circleStyle.append("-fx-fill: black; -fx-stroke: white");
+        triangleStyle.append("-fx-stroke: white");
 
-        circle.setRadius(10);
-        circle.setCenterX(_referenceX + point.x);
-        circle.setCenterY(_referenceY + point.y);
+        double ankathete = 15; // Seiten beziehen sich nicht auf das ganze gleichseitige Dreieck, sondern auf ein inneres kleineres
+        double gegenkathete = Math.tan(Math.toRadians(30)) * ankathete;
+        double hypotenuse = Math.sqrt(Math.pow(10,2)+Math.pow(gegenkathete,2));
+        double x = _referenceX + point.x;
+        double y = _referenceY + point.y;
+
+        triangle.getPoints().addAll(x,y - hypotenuse, //top corner
+                x + ankathete, y + gegenkathete, //bottom right
+                x - ankathete, y + gegenkathete); //bottom left
 
         String sID = String.valueOf(mID);
         Text text = new Text();
@@ -42,7 +48,7 @@ public class BeidouSat extends SatelliteInfo{
         text.setStyle("-fx-stroke: white;");
         text.setTextOrigin(VPos.CENTER);
 
-        circle.setStyle(circleStyle.toString());
-        _parent.getChildren().addAll(circle, text);
+        triangle.setStyle(triangleStyle.toString());
+        _parent.getChildren().addAll(triangle, text);
     }
 }
