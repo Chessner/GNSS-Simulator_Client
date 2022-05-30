29f3ed9a84fdd8b853688bb60c39721d36529fb8
Chessner <s2010237006@students.fh-hagenberg.at>
2022-05-30 17:34:28 +0200

    Galileo satellites now with distinct look

--- src/at/fh/hgb/mc/GalileoSat.java
+++ src/at/fh/hgb/mc/GalileoSat.java
@@ -2,47 +2,52 @@ package at.fh.hgb.mc;
 
 import javafx.geometry.VPos;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 
 import java.awt.geom.Point2D;
 
-public class GalileoSat extends SatelliteInfo{
+public class GalileoSat extends SatelliteInfo {
     @Override
     public void createShape(Pane _parent, double _referenceX, double _referenceY, double _radius) {
         Point2D.Double point = getCoordinates(_radius);
-        Circle circle = new Circle();
-        circle.setId("Satellite");
-        StringBuilder circleStyle = new StringBuilder();
+        Polygon stopSign = new Polygon();
+        stopSign.setId("Satellite");
+        StringBuilder stopSignStyle = new StringBuilder();
 
         if (mSNRdB == Integer.MIN_VALUE) {
             //no snr value exists (marked with Integer.MIN_VALUE)
-            circleStyle.append("-fx-fill: red; ");
+            stopSignStyle.append("-fx-fill: red; ");
         } else if (mParentNMEAInfo.mIDsSatellitesUsed.contains(mID)) {
             //satellite was used
-            circleStyle.append("-fx-fill: green; ");
+            stopSignStyle.append("-fx-fill: green; ");
         } else {
             //satellite was not used
-            circleStyle.append("-fx-fill: #0000");
-            circleStyle.append(Integer.toHexString((int) Math.max(255, Math.min(0, mSNRdB))));
-            circleStyle.append("ff; ");
+            stopSignStyle.append("-fx-fill: #0000");
+            stopSignStyle.append(Integer.toHexString((int) Math.max(255, Math.min(0, mSNRdB))));
+            stopSignStyle.append("ff; ");
         }
-        circleStyle.append("-fx-fill: brown; -fx-stroke: white");
+        stopSignStyle.append("-fx-stroke: white");
 
-        circle.setRadius(10);
-        circle.setCenterX(_referenceX + point.x);
-        circle.setCenterY(_referenceY + point.y);
+        stopSign.getPoints().addAll(_referenceX + point.x - 10, _referenceY + point.y - 5,
+                _referenceX + point.x - 5, _referenceY + point.y - 10,
+                _referenceX + point.x + 5, _referenceY + point.y - 10,
+                _referenceX + point.x + 10, _referenceY + point.y - 5,
+                _referenceX + point.x + 10, _referenceY + point.y + 5,
+                _referenceX + point.x + 5, _referenceY + point.y + 10,
+                _referenceX + point.x - 5, _referenceY + point.y + 10,
+                _referenceX + point.x - 10, _referenceY + point.y + 5);
 
         String sID = String.valueOf(mID);
         Text text = new Text();
         text.setId("Satellite");
-        text.setX(_referenceX + point.x - sID.length()*3);
+        text.setX(_referenceX + point.x - sID.length() * 3);
         text.setY(_referenceY + point.y);
         text.setText(sID);
         text.setStyle("-fx-stroke: white;");
         text.setTextOrigin(VPos.CENTER);
 
-        circle.setStyle(circleStyle.toString());
-        _parent.getChildren().addAll(circle, text);
+        stopSign.setStyle(stopSignStyle.toString());
+        _parent.getChildren().addAll(stopSign, text);
     }
 }
